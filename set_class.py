750a2a7a658d010da44af0b1c3a9fea9d9443b2d
Maeson <maesoneser1219@gmail.com>
2026-01-21 09:12:04 -0500

    outlined set class

--- set_class.py
+++ set_class.py
@@ -2,3 +2,70 @@
 # Unit 3 Lab 7
 # The Set Class
 
+class Set:
+    def __init__(self):
+        self.set = []
+        self.size = 0
+    
+    def add(self,element):
+        pass
+
+    def clear(self):
+        pass
+
+    def remove(self,element):
+        pass
+
+    def discard(self,element):
+        pass
+
+    def union(self,other):
+        pass
+
+    def intersection(self,other):
+        pass
+
+    def difference(self,other):
+        pass
+
+    def is_subset(self,other):
+        pass
+    
+    def has_element(self,element):
+        pass
+
+    def is_disjoint(self,other):
+        pass
+
+    def power_set(self):
+        pass
+
+    def cartesian_product(self,other):
+        pass
+
+    def __contains__(self,element):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __eq__(self,other):
+        pass
+
+    def __ne__(self,other):
+        pass
+
+    def __lt__(self,other):
+        pass
+
+    def __gt__(self,other):
+        pass
+
+    def __le__(self,other):
+        pass
+
+    def __ge__(self,other):
+        pass
+    
+    def __iter__(self):
+        pass
